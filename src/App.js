de79676b50e3a4894b36a787767d0919bdf8017c
M.A.Garcia <m.angel.gf04.com>
2020-06-29 19:18:41 +0200

    withRouter in Homecontiner + BrowserRouter + Switch

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import './App.css';
 import HomeContainer from './containers/HomeContainer';
 
 class App extends Component {
+
+  renderHome = () => <HomeContainer />;
+
+  renderCustomerContainer = () => <h1>Customers Container</h1>;
+
+  renderCustomerListContainer = () => <h1>Customers List  Container</h1>;
+
+  renderCustomerNewContainer = () => <h1>Customers New Container</h1>;
+
+
+
   render() {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={HomeContainer} />
-          <Route exact path='/customers' component={HomeContainer} />
+          <Route exact path='/' component={this.renderHome} />
+          <Route exact path='/customers' component={this.renderCustomerListContainer} />
+          <Switch>
+            <Route path='/cutomers/new' component={this.renderCustomerNewContainer} />
+            <Route path='/customers/:dni' component={this.renderCustomerContainer} />
+          </Switch>
         </div>
       </Router>
     );
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import CustomersActions from '../components/CustomersActions';
+import { withRouter } from 'react-router-dom';
 
 class HomeContainer extends Component {
 
@@ -32,4 +33,4 @@ HomeContainer.propTypes = {
 
 };
 
-export default HomeContainer;
+export default withRouter(HomeContainer);
